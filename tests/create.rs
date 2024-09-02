1296761cdbd8ae15c4d7f0cdb55fbe3b06e2ac80
Casey Rodarmor <casey@rodarmor.com>
2024-09-01 19:08:58 -0700

    Another test

--- tests/create.rs
+++ tests/create.rs
@@ -45,3 +45,28 @@ fn single_file() -> Result {
 
   Ok(())
 }
+
+#[test]
+fn file_in_subdirectory() -> Result {
+  let dir = TempDir::new()?;
+
+  dir.child("foo/bar").touch().unwrap();
+
+  Command::cargo_bin("filepack")?
+    .args(["create", "."])
+    .current_dir(&dir)
+    .assert()
+    .success();
+
+  dir.child("filepack.json").assert(
+    r#"{"files":{"foo/bar":"af1349b9f5f9a1a6a0404dea36dcc9499bcb25c9adc112b7cc9a93cae41f3262"}}"#,
+  );
+
+  Command::cargo_bin("filepack")?
+    .args(["verify", "."])
+    .current_dir(&dir)
+    .assert()
+    .success();
+
+  Ok(())
+}
