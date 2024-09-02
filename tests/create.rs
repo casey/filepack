0ffc0f4a43dfe1e10c1480b324ac0f8765871603
Casey Rodarmor <casey@rodarmor.com>
2024-09-01 18:59:45 -0700

    add single file test

--- tests/create.rs
+++ tests/create.rs
@@ -20,3 +20,28 @@ fn no_files() -> Result {
 
   Ok(())
 }
+
+#[test]
+fn single_file() -> Result {
+  let dir = TempDir::new()?;
+
+  dir.child("foo").touch().unwrap();
+
+  Command::cargo_bin("filepack")?
+    .args(["create", "."])
+    .current_dir(&dir)
+    .assert()
+    .success();
+
+  dir.child("filepack.json").assert(
+    r#"{"files":{"foo":"af1349b9f5f9a1a6a0404dea36dcc9499bcb25c9adc112b7cc9a93cae41f3262"}}"#,
+  );
+
+  Command::cargo_bin("filepack")?
+    .args(["verify", "."])
+    .current_dir(&dir)
+    .assert()
+    .success();
+
+  Ok(())
+}
