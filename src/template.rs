6589765f9b0882cb760bcf31de0e454c27ad7c5f
Casey Rodarmor <casey@rodarmor.com>
2024-09-15 22:36:20 +0700

    Use kebab-case field names

--- src/template.rs
+++ src/template.rs
@@ -1,7 +1,7 @@
 use super::*;
 
 #[derive(Deserialize)]
-#[serde(deny_unknown_fields)]
+#[serde(deny_unknown_fields, rename_all = "kebab-case")]
 pub(crate) struct Template {
   pub(crate) title: String,
 }
