ac27e5af6e545ce2349eee35cb78c429d284b6b8
Casey Rodarmor <casey@rodarmor.com>
2024-10-07 00:47:58 -0700

    Simplifying error handling

--- src/github_release.rs
+++ src/github_release.rs
@@ -21,14 +21,8 @@ impl FromStr for GithubRelease {
   fn from_str(s: &str) -> Result<Self, Self::Err> {
     let components = s
       .split('/')
-      .map(|component| {
-        if component.is_empty() {
-          Err(ERROR)
-        } else {
-          Ok(component)
-        }
-      })
-      .collect::<Result<Vec<&str>, &str>>()?;
+      .filter(|component| !component.is_empty())
+      .collect::<Vec<&str>>();
 
     if components.len() != 3 {
       return Err(ERROR);
@@ -63,10 +57,12 @@ mod tests {
   #[test]
   fn err() {
     "".parse::<GithubRelease>().unwrap_err();
+    "/".parse::<GithubRelease>().unwrap_err();
+    "//".parse::<GithubRelease>().unwrap_err();
     "///".parse::<GithubRelease>().unwrap_err();
     "a//".parse::<GithubRelease>().unwrap_err();
     "/b/".parse::<GithubRelease>().unwrap_err();
-    "/b/c".parse::<GithubRelease>().unwrap_err();
+    "//c".parse::<GithubRelease>().unwrap_err();
     "a/b/c/d".parse::<GithubRelease>().unwrap_err();
   }
 }
