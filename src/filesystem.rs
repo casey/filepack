ce4ae18e9d313133d3792d1e716f749961adb62a
Casey Rodarmor <casey@rodarmor.com>
2024-10-10 20:53:33 -0700

    Reform

---
+++ src/filesystem.rs
@@ -0,0 +1,13 @@
+use super::*;
+
+pub(crate) fn read_to_string(path: &Utf8Path) -> Result<String> {
+  std::fs::read_to_string(path).context(error::Io { path })
+}
+
+pub(crate) fn metadata(path: &Utf8Path) -> Result<std::fs::Metadata> {
+  std::fs::metadata(path).context(error::Io { path })
+}
+
+pub(crate) fn write(path: &Utf8Path, contents: &[u8]) -> Result {
+  std::fs::write(path, contents).context(error::Io { path })
+}
