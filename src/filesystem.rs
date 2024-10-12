bee87c58336de267710b4ba323c498e47fa6e788
Casey Rodarmor <casey@rodarmor.com>
2024-10-11 20:02:36 -0700

    Tweak

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -4,6 +4,14 @@ pub(crate) fn create_dir_all(path: &Utf8Path) -> Result<()> {
   std::fs::create_dir_all(path).context(error::Io { path })
 }
 
+pub(crate) fn exists(path: &Utf8Path) -> Result<bool> {
+  path.try_exists().context(error::Io { path })
+}
+
+pub(crate) fn metadata(path: &Utf8Path) -> Result<std::fs::Metadata> {
+  std::fs::metadata(path).context(error::Io { path })
+}
+
 pub(crate) fn read_to_string(path: impl AsRef<Utf8Path>) -> Result<String> {
   std::fs::read_to_string(path.as_ref()).context(error::Io {
     path: path.as_ref(),
@@ -17,14 +25,6 @@ pub(crate) fn read_to_string_opt(path: &Utf8Path) -> Result<Option<String>> {
   }
 }
 
-pub(crate) fn metadata(path: &Utf8Path) -> Result<std::fs::Metadata> {
-  std::fs::metadata(path).context(error::Io { path })
-}
-
 pub(crate) fn write(path: &Utf8Path, contents: impl AsRef<[u8]>) -> Result {
   std::fs::write(path, contents).context(error::Io { path })
-}
-
-pub(crate) fn exists(path: &Utf8Path) -> Result<bool> {
-  path.try_exists().context(error::Io { path })
 }
