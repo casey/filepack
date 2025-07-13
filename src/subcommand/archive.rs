fb0d08700c26f63ee19514c1e81aef9396612ee3
Casey Rodarmor <casey@rodarmor.com>
2025-07-12 18:30:01 -0700

    archive_path -> path

--- src/subcommand/archive.rs
+++ src/subcommand/archive.rs
@@ -7,6 +7,8 @@ const MAGIC_BYTES: &[u8] = b"FILEPACK";
 //   - call it a file signature instead? more self-explanatory
 //   - include non text characters
 //   - add more entropy
+//   - add a version number?
+//   - use emoji because fun?
 // - assert that content has correct size and hash
 
 pub(crate) fn run() -> Result {
@@ -26,17 +28,13 @@ pub(crate) fn run() -> Result {
 
   files.sort_by_key(|(hash, _content)| *hash);
 
-  let archive_path = "archive.filepack";
+  let path = "archive.filepack";
 
-  let archive = File::create("archive.filepack").context(error::Io { path: archive_path })?;
+  let archive = File::create(path).context(error::Io { path })?;
 
   let mut writer = BufWriter::new(archive);
 
-  let mut write = |data: &[u8]| {
-    writer
-      .write_all(data)
-      .context(error::Io { path: archive_path })
-  };
+  let mut write = |data: &[u8]| writer.write_all(data).context(error::Io { path });
 
   write(MAGIC_BYTES)?;
 
