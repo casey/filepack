7b04f5bee24331a684c29d6a359399f5bcdbd20e
Casey Rodarmor <casey@rodarmor.com>
2026-01-06 23:02:30 -0800

    Adjust

--- tests/verify.rs
+++ tests/verify.rs
@@ -338,7 +338,7 @@ fn non_unicode_path_error() {
     .args(["verify", "."])
     .current_dir(&dir)
     .assert()
-    .stderr(format!("error: path not valid unicode: `.{SEPARATOR}�`\n"))
+    .stderr(path("error: path not valid unicode: `./�`\n"))
     .failure();
 }
 
