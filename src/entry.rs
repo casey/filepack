9304c1e7576fb20d2818dca1dc64a6cc37c6ebb1
Casey Rodarmor <casey@rodarmor.com>
2026-01-09 20:18:05 -0800

    Hash variable-length fields when fingerprinting

--- src/entry.rs
+++ src/entry.rs
@@ -11,14 +11,14 @@ impl Entry {
   pub(crate) fn fingerprint(&self, component: &Component) -> Hash {
     let mut hasher = FieldHasher::new(Context::Entry);
 
-    hasher.field(0, component.as_bytes());
+    hasher.field(0, Hash::bytes(component.as_bytes()));
 
     let inner = match self {
       Self::Directory(directory) => directory.fingerprint(),
       Self::File(file) => file.fingerprint(),
     };
 
-    hasher.field(1, inner.as_bytes());
+    hasher.field(1, inner);
 
     hasher.finalize()
   }
