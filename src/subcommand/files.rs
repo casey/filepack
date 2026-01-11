c1932347595101debe5d6f6992e894dfd99d639c
Casey Rodarmor <casey@rodarmor.com>
2026-01-10 21:57:53 -0800

    Add json and tsv formats options to `filepack files`

--- src/subcommand/files.rs
+++ src/subcommand/files.rs
@@ -1,19 +1,35 @@
 use super::*;
 
+#[derive(Clone, ValueEnum)]
+enum Format {
+  Json,
+  JsonPretty,
+  Tsv,
+}
+
 #[derive(Parser)]
 pub(crate) struct Files {
   #[arg(help = MANIFEST_PATH_HELP)]
   path: Option<Utf8PathBuf>,
+  #[arg(long = "format", default_value = "json-pretty")]
+  format: Format,
 }
 
 impl Files {
   pub(crate) fn run(self) -> Result {
     let (_path, manifest) = Manifest::load(self.path.as_deref())?;
 
-    println!(
-      "{}",
-      serde_json::to_string_pretty(&manifest.files()).unwrap(),
-    );
+    let files = manifest.files();
+
+    match self.format {
+      Format::Json => println!("{}", serde_json::to_string(&files).unwrap(),),
+      Format::JsonPretty => println!("{}", serde_json::to_string_pretty(&files).unwrap(),),
+      Format::Tsv => {
+        for (path, file) in files {
+          println!("{path}\t{}\t{}", file.hash, file.size);
+        }
+      }
+    }
 
     Ok(())
   }
--- tests/files.rs
+++ tests/files.rs
@@ -1,7 +1,7 @@
 use super::*;
 
 #[test]
-fn files() {
+fn files_default() {
   let dir = TempDir::new().unwrap();
 
   dir.child("foo/bar/baz").touch().unwrap();
@@ -28,5 +28,62 @@ fn files() {
         size: 0,
       },
     })
+    .success();
+}
+
+#[test]
+fn files_json() {
+  let dir = TempDir::new().unwrap();
+
+  dir.child("foo/bar/baz").touch().unwrap();
+
+  dir.child("foo/bar/bob").touch().unwrap();
+
+  cargo_bin_cmd!("filepack")
+    .args(["create", "."])
+    .current_dir(&dir)
+    .assert()
+    .success();
+
+  cargo_bin_cmd!("filepack")
+    .args(["files", "--format", "json"])
+    .current_dir(&dir)
+    .assert()
+    .stdout(json! {
+      "foo/bar/baz": {
+        hash: EMPTY_HASH,
+        size: 0,
+      },
+      "foo/bar/bob": {
+        hash: EMPTY_HASH,
+        size: 0,
+      },
+    })
+    .success();
+}
+
+#[test]
+fn files_tsv() {
+  let dir = TempDir::new().unwrap();
+
+  dir.child("foo/bar/baz").touch().unwrap();
+
+  dir.child("foo/bar/bob").touch().unwrap();
+
+  cargo_bin_cmd!("filepack")
+    .args(["create", "."])
+    .current_dir(&dir)
+    .assert()
+    .success();
+
+  cargo_bin_cmd!("filepack")
+    .args(["files", "--format", "tsv"])
+    .current_dir(&dir)
+    .assert()
+    .stdout(format!(
+      "foo/bar/baz\t{EMPTY_HASH}\t0
+foo/bar/bob\t{EMPTY_HASH}\t0
+"
+    ))
     .success();
 }
