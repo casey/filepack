a97f4b990d846c89b0ee3d3e7a036fb6893411c2
Casey Rodarmor <casey@rodarmor.com>
2026-01-14 11:05:21 -0800

    Restrict key names (#131)

--- src/key_name.rs
+++ src/key_name.rs
@@ -1,7 +1,6 @@
 use super::*;
 
-static NAME_RE: LazyLock<Regex> =
-  LazyLock::new(|| Regex::new("^[A-Za-z0-9][A-Za-z0-9._-]*$").unwrap());
+static NAME_RE: LazyLock<Regex> = LazyLock::new(|| Regex::new("^[a-z0-9]+(-[a-z0-9]+)*$").unwrap());
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct KeyName(String);
@@ -20,7 +19,7 @@ impl FromStr for KeyName {
   type Err = PublicKeyError;
 
   fn from_str(name: &str) -> Result<Self, Self::Err> {
-    if !NAME_RE.is_match(name) {
+    if !NAME_RE.is_match(name) || name.len() > 128 {
       return Err(public_key_error::NameError { name }.build());
     }
 
@@ -31,5 +30,47 @@ impl FromStr for KeyName {
 impl Display for KeyName {
   fn fmt(&self, f: &mut Formatter) -> fmt::Result {
     write!(f, "{}", self.0)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn validity() {
+    #[track_caller]
+    fn valid(s: &str) {
+      assert!(s.parse::<KeyName>().is_ok());
+    }
+
+    #[track_caller]
+    fn invalid(s: &str) {
+      assert!(s.parse::<KeyName>().is_err());
+    }
+
+    valid("0");
+    valid("0-0");
+    valid("foo");
+    valid("foo-bar");
+    valid("foo-bar-baz");
+    valid(&"a".repeat(128));
+
+    invalid("");
+    invalid("-");
+    invalid("--");
+    invalid(".");
+    invalid("..");
+    invalid("/");
+    invalid("FOO");
+    invalid("\\");
+    invalid("_");
+    invalid("foo--bar");
+    invalid("foo--bar");
+    invalid("foo-bar-");
+    invalid("foo.");
+    invalid("foo.bar");
+    invalid("foo_bar");
+    invalid(&"a".repeat(129));
   }
 }
