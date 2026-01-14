1eeb95ae182c69c27e1cd69adad94c10f8040ee1
Casey Rodarmor <casey@rodarmor.com>
2026-01-13 21:05:03 -0800

    Modify

---
+++ src/mode.rs
@@ -0,0 +1,42 @@
+use super::*;
+
+#[derive(Clone, Copy, Debug)]
+pub struct Mode(u32);
+
+impl Mode {
+  pub(crate) fn is_secure(self) -> bool {
+    self.0.trailing_zeros() >= 6
+  }
+}
+
+impl Display for Mode {
+  fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+    write!(f, "0{:03o}", self.0 & 0o777)
+  }
+}
+
+impl From<Permissions> for Mode {
+  fn from(permissions: Permissions) -> Self {
+    #[cfg(unix)]
+    let mode = {
+      use std::os::unix::fs::PermissionsExt;
+      permissions.mode()
+    };
+
+    #[cfg(not(unix))]
+    let mode = 0;
+
+    Self(mode)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn display() {
+    assert_eq!(Mode(0).to_string(), "0000");
+    assert_eq!(Mode(0o777).to_string(), "0777");
+  }
+}
