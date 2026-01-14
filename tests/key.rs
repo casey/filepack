b2fb0e6302e032ac9c19ca9f0405db53e87c2b08
Casey Rodarmor <casey@rodarmor.com>
2026-01-14 00:49:38 -0800

    Revise

--- tests/key.rs
+++ tests/key.rs
@@ -52,3 +52,47 @@ fn prints_pubkey() {
     .stdout(format!("{public_key}\n"))
     .success();
 }
+
+#[test]
+fn prints_named_key() {
+  let test = Test::new()
+    .args(["keygen", "--name", "deploy"])
+    .success()
+    .args(["key", "--key", "deploy"])
+    .stdout_regex("[0-9a-f]{64}\n")
+    .success();
+
+  let public_key = test.read("keys/deploy.public");
+
+  test
+    .args(["key", "--key", "deploy"])
+    .stdout(format!("{public_key}\n"))
+    .success();
+}
+
+#[test]
+fn explicit_master_key() {
+  let test = Test::new()
+    .args(["keygen"])
+    .success()
+    .args(["key", "--key", "master"])
+    .stdout_regex("[0-9a-f]{64}\n")
+    .success();
+
+  let public_key = test.read("keys/master.public");
+
+  test
+    .args(["key", "--key", "master"])
+    .stdout(format!("{public_key}\n"))
+    .success();
+}
+
+#[test]
+fn nonexistent_key_error() {
+  Test::new()
+    .args(["keygen"])
+    .success()
+    .args(["key", "--key", "nonexistent"])
+    .stderr_regex("error: public key not found: `.*nonexistent.public`\n")
+    .failure();
+}
