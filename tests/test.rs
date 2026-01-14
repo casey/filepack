454f09a129198d4bc606e545949739348479a65c
Casey Rodarmor <casey@rodarmor.com>
2026-01-13 18:47:18 -0800

    Enhance

--- tests/test.rs
+++ tests/test.rs
@@ -176,6 +176,7 @@ impl Test {
     let output = child.wait_with_output().unwrap();
 
     let stdout = str::from_utf8(&output.stdout).unwrap();
+
     let stderr = str::from_utf8(&output.stderr).unwrap();
 
     if code == 0 && !output.status.success() {
