e1c94d55f35f7ca2cc9d30b5c78e77e241923f3a
Casey Rodarmor <casey@rodarmor.com>
2026-01-17 01:02:10 -0800

    Move metadata tests into dedicated module

---
+++ tests/metadata.rs
@@ -0,0 +1,112 @@
+use super::*;
+
+#[test]
+fn files_missing() {
+  #[track_caller]
+  fn case(metadata: &str, stderr: &str) {
+    Test::new()
+      .write("metadata.yaml", metadata)
+      .arg("create")
+      .stderr_regex(stderr)
+      .failure();
+  }
+
+  case(
+    "title: Foo\nartwork: cover.png",
+    "error: file referenced in metadata missing: `cover.png`\n",
+  );
+
+  case(
+    "title: Foo\nnfo: info.nfo",
+    "error: file referenced in metadata missing: `info.nfo`\n",
+  );
+
+  case(
+    "title: Foo\nreadme: README.md",
+    "error: file referenced in metadata missing: `README.md`\n",
+  );
+}
+
+#[test]
+fn files_wrong_extension() {
+  #[track_caller]
+  fn case(metadata: &str, file: &str, stderr: &str) {
+    Test::new()
+      .touch(file)
+      .write("metadata.yaml", metadata)
+      .arg("create")
+      .stderr_regex(stderr)
+      .failure();
+  }
+
+  case(
+    "title: Foo\nartwork: cover.jpg",
+    "cover.jpg",
+    ".*component must end in `.png`.*",
+  );
+
+  case(
+    "title: Foo\nnfo: info.txt",
+    "info.txt",
+    ".*component must end in `.nfo`.*",
+  );
+
+  case(
+    "title: Foo\nreadme: README.txt",
+    "README.txt",
+    ".*component must end in `.md`.*",
+  );
+}
+
+#[test]
+fn invalid_language() {
+  Test::new()
+    .write("metadata.yaml", "title: Foo\nlanguage: ac")
+    .arg("create")
+    .stderr_regex(".*unknown language code `ac`.*")
+    .failure();
+}
+
+#[test]
+fn unknown_keys() {
+  Test::new()
+    .write("metadata.yaml", "title: Foo\nbar: baz")
+    .arg("create")
+    .stderr_regex(".*unknown fields in metadata at `.*metadata.yaml`: `bar`\n")
+    .failure();
+}
+
+#[test]
+fn files() {
+  Test::new()
+    .touch("content")
+    .touch("cover.png")
+    .touch("info.nfo")
+    .touch("README.md")
+    .write(
+      "metadata.yaml",
+      "\
+title: Foo
+artwork: cover.png
+nfo: info.nfo
+readme: README.md
+",
+    )
+    .arg("create")
+    .success()
+    .arg("verify")
+    .stderr("successfully verified 5 files totaling 62 bytes\n")
+    .success();
+}
+
+#[test]
+fn language() {
+  Test::new()
+    .touch("content")
+    .write("metadata.yaml", "title: Foo\nlanguage: en")
+    .arg("create")
+    .success()
+    .arg("verify")
+    .stderr("successfully verified 2 files totaling 23 bytes\n")
+    .success();
+}
