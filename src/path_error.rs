be69a0555f05752e311889501bb372044ba04635
Casey Rodarmor <casey@rodarmor.com>
2026-01-16 20:47:26 -0800

    Tweak

--- src/path_error.rs
+++ src/path_error.rs
@@ -6,16 +6,16 @@ pub enum PathError {
   Component { component: String },
   #[snafu(display("paths may not contain empty components"))]
   ComponentEmpty,
-  #[snafu(display("component exceeds 255 byte limit"))]
-  Length,
   #[snafu(display("paths may not contain double slashes"))]
   DoubleSlash,
   #[snafu(display("paths may not be empty"))]
   Empty,
-  #[snafu(display("paths may not contain NUL character"))]
-  Nul,
   #[snafu(display("paths may not begin with slash character"))]
   LeadingSlash,
+  #[snafu(display("component exceeds 255 byte limit"))]
+  Length,
+  #[snafu(display("paths may not contain NUL character"))]
+  Nul,
   #[snafu(display("paths may not contain separator character `{character}`"))]
   Separator { character: char },
   #[snafu(display("paths may not end with slash character"))]
