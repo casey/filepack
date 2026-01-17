49694ae6669828b7f9db140fcfc259ca90ea9e62
Casey Rodarmor <casey@rodarmor.com>
2026-01-16 20:09:45 -0800

    Check for `\n` in packager

--- src/packager.rs
+++ src/packager.rs
@@ -1,12 +1,22 @@
 use super::*;
 
+#[derive(Debug, Snafu)]
+pub enum PackagerError {
+  #[snafu(display("packager may not contain `{character}`"))]
+  Character { character: char },
+}
+
 #[derive(Clone, Debug, DeserializeFromStr, PartialEq)]
 pub(crate) struct Packager(String);
 
 impl FromStr for Packager {
-  type Err = std::convert::Infallible;
+  type Err = PackagerError;
 
   fn from_str(s: &str) -> Result<Self, Self::Err> {
+    if s.contains('\n') {
+      return Err(PackagerError::Character { character: '\n' });
+    }
+
     Ok(Self(s.into()))
   }
 }
