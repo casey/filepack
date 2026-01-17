c344a55f27445fdeaf01f122f47c24fc92d2977b
Casey Rodarmor <casey@rodarmor.com>
2026-01-17 00:48:55 -0800

    Adjust

--- tests/languages.rs
+++ tests/languages.rs
@@ -1,9 +1,25 @@
 use super::*;
 
 #[test]
-fn languages() {
+fn default() {
   Test::new()
     .arg("languages")
     .stdout_regex("\\{\n.*\"en\": \"English\".*}\n")
+    .success();
+}
+
+#[test]
+fn json() {
+  Test::new()
+    .args(["languages", "--format", "json"])
+    .stdout_regex("\\{.*\"en\":\"English\".*}\n")
+    .success();
+}
+
+#[test]
+fn tsv() {
+  Test::new()
+    .args(["languages", "--format", "tsv"])
+    .stdout_regex(".*en\tEnglish\n.*")
     .success();
 }
