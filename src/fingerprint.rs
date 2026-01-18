909b4620ca1b5faa0d122975f51e61142d6253f2
Casey Rodarmor <casey@rodarmor.com>
2026-01-17 22:29:16 -0800

    Adapt

--- src/fingerprint.rs
+++ src/fingerprint.rs
@@ -1,6 +1,6 @@
 use super::*;
 
-#[derive(Clone, Copy, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Fingerprint(pub(crate) Hash);
 
 impl Fingerprint {
