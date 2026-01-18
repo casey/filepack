51d9b2eedab53eb0fd15537e966c5fb9ac9b2460
Casey Rodarmor <casey@rodarmor.com>
2026-01-17 16:08:49 -0800

    Add creator metadata (#165)

--- src/package.rs
+++ src/package.rs
@@ -3,8 +3,8 @@ use super::*;
 #[derive(Clone, Debug, Deserialize, PartialEq)]
 #[serde(rename_all = "kebab-case")]
 pub(crate) struct Package {
+  pub(crate) creator: Option<Component>,
   pub(crate) date: Option<DateTime>,
   pub(crate) description: Option<String>,
   pub(crate) nfo: Option<filename::Nfo>,
-  pub(crate) packager: Option<Component>,
 }
