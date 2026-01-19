aef37a3792fe8e93b556eb19b53aa0cde1c634c0
Casey Rodarmor <casey@rodarmor.com>
2026-01-18 18:28:29 -0800

    Tweak

--- src/re.rs
+++ src/re.rs
@@ -9,6 +9,6 @@ macro_rules! re {
 
 re! { DATE,       r"\d\d\d\d-\d\d-\d\d"      }
 re! { KEY_NAME,   r"[0-9a-z]+(-[0-9a-z]+)*"  }
-re! { PUBLIC_KEY, r"public1[a-z0-9]+"        }
+re! { PUBLIC_KEY, r"public1.*"               }
 re! { TAG,        r"[0-9A-Z]+(\.[0-9A-Z]+)*" }
 re! { YEAR,       r"[1-9][0-9]{0,3}"         }
