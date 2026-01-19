089cdb8c5ae6815ee8f4ec336f106d981a4588fa
Casey Rodarmor <casey@rodarmor.com>
2026-01-18 20:53:39 -0800

    Adapt

--- src/subcommand/bech32m.rs
+++ src/subcommand/bech32m.rs
@@ -5,19 +5,19 @@ use super::*;
 pub(crate) struct Bech32m {
   #[arg(
     group = "source",
+    help = "Convert <BECH32M> to hexadecimal",
+    long,
+    value_name = "BECH32M"
+  )]
+  decode: Option<String>,
+  #[arg(
+    group = "source",
     help = "Encode <HEXADECIMAL> to bech32m",
     long,
     requires = "hrp",
     value_name = "HEXADECIMAL"
   )]
   encode: Option<String>,
-  #[arg(
-    group = "source",
-    help = "Convert <BECH32M> to hexadecimal",
-    long,
-    value_name = "BECH32M"
-  )]
-  decode: Option<String>,
   #[arg(help = "Prefix bech32m with human-readable part <HRP>", long)]
   hrp: Option<String>,
 }
@@ -32,7 +32,7 @@ impl Bech32m {
       println!("{hex}");
     } else {
       let hrp = Hrp::parse(&self.hrp.unwrap()).context(error::Bech32mHrp)?;
-      let hex = hex::decode(&self.encode.unwrap()).context(error::Hex)?;
+      let hex = hex::decode(self.encode.unwrap()).context(error::Hex)?;
       let bech32m = bech32::encode::<bech32::Bech32m>(hrp, &hex).context(error::Bech32mEncode)?;
       println!("{bech32m}");
     }
