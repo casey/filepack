bdaaae2852154b4713c74f0580bf24d5d8e388a9
Casey Rodarmor <casey@rodarmor.com>
2026-01-18 21:00:51 -0800

    Revise

--- tests/bech32m.rs
+++ tests/bech32m.rs
@@ -9,11 +9,35 @@ fn decode() {
 }
 
 #[test]
+fn decode_invalid_bech32m() {
+  Test::new()
+    .args(["bech32m", "--decode", "invalid"])
+    .stderr_regex("error: failed to decode bech32m `invalid`\n.*")
+    .failure();
+}
+
+#[test]
 fn encode() {
   Test::new()
     .args(["bech32m", "--encode", "666f6f", "--hrp", "foo"])
     .stdout("foo1vehk7e3xjxp\n")
     .success();
+}
+
+#[test]
+fn encode_invalid_hex() {
+  Test::new()
+    .args(["bech32m", "--encode", "gg", "--hrp", "foo"])
+    .stderr_regex("error: failed to parse hexadecimal `gg`\n.*")
+    .failure();
+}
+
+#[test]
+fn encode_invalid_hrp() {
+  Test::new()
+    .args(["bech32m", "--encode", "666f6f", "--hrp", " "])
+    .stderr_regex("error: failed to parse bech32m human-readable part\n.*")
+    .failure();
 }
 
 #[test]
@@ -32,28 +56,4 @@ fn source_required() {
       "error: the following required arguments were not provided:.*--decode.*--encode.*",
     )
     .status(2);
-}
-
-#[test]
-fn decode_invalid_bech32m() {
-  Test::new()
-    .args(["bech32m", "--decode", "invalid"])
-    .stderr_regex("error: failed to decode bech32m `invalid`\n.*")
-    .failure();
-}
-
-#[test]
-fn encode_invalid_hex() {
-  Test::new()
-    .args(["bech32m", "--encode", "gg", "--hrp", "foo"])
-    .stderr_regex("error: failed to parse hexadecimal `gg`\n.*")
-    .failure();
-}
-
-#[test]
-fn encode_invalid_hrp() {
-  Test::new()
-    .args(["bech32m", "--encode", "666f6f", "--hrp", " "])
-    .stderr_regex("error: failed to parse bech32m human-readable part\n.*")
-    .failure();
 }
