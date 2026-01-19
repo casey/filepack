c02ace789725bf9e8e733419eb9f449638406093
Casey Rodarmor <casey@rodarmor.com>
2026-01-18 18:09:36 -0800

    Adjust

--- src/bech32m_error.rs
+++ src/bech32m_error.rs
@@ -18,33 +18,3 @@ pub enum Bech32mError {
   #[snafu(display("expected {expected} bytes but found {actual}"))]
   Length { expected: usize, actual: usize },
 }
-
-#[cfg(test)]
-mod tests {
-  use super::*;
-
-  #[test]
-  fn display() {
-    #[track_caller]
-    fn case(s: &str, expected: &str) {
-      assert_eq!(
-        PublicKey::decode_bech32m(s).unwrap_err().to_string(),
-        expected,
-      );
-    }
-
-    case("foo", "failed to decode bech32m public key");
-
-    case(
-      test::PRIVATE_KEY,
-      "expected bech32m human-readable prefix `public1...` but found `private1...`",
-    );
-
-    case("public134jkgz", "expected 32 bytes but found 0");
-
-    case(
-      "public1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq6s7wps",
-      "expected 32 bytes but found 33",
-    );
-  }
-}
