e6aa53428a1b62f808e0726e0ad85a23ca1c592b
Casey Rodarmor <casey@rodarmor.com>
2026-01-20 23:24:23 -0800

    Finish test

--- src/pgp.rs
+++ src/pgp.rs
@@ -1,8 +1,119 @@
+use {
+  super::*,
+  sequoia_openpgp::{
+    self as openpgp,
+    cert::prelude::*,
+    crypto::mpi,
+    packet::signature::SignatureBuilder,
+    policy::StandardPolicy,
+    serialize::MarshalInto,
+    types::{HashAlgorithm, SignatureType},
+  },
+  sha2::{Digest, Sha512},
+};
+
 #[test]
 fn pgp_signatures_can_be_verified() {
   if cfg!(windows) {
     return;
   }
 
-  todo!()
+  let manifest = Manifest {
+    files: Directory::new(),
+    notes: Vec::new(),
+  };
+
+  let message = Message {
+    fingerprint: manifest.fingerprint(),
+    time: None,
+  };
+
+  let message = message.serialize();
+
+  let policy = StandardPolicy::new();
+
+  let (cert, _revocation) = CertBuilder::new()
+    .add_userid("foo@bar")
+    .add_signing_subkey()
+    .generate()
+    .unwrap();
+
+  let signing_key = cert
+    .keys()
+    .unencrypted_secret()
+    .with_policy(&policy, None)
+    .supported()
+    .alive()
+    .revoked(false)
+    .for_signing()
+    .next()
+    .unwrap();
+
+  let mut keypair = signing_key.key().clone().into_keypair().unwrap();
+
+  let signature_packet = SignatureBuilder::new(SignatureType::Binary)
+    .set_hash_algo(HashAlgorithm::SHA512)
+    .sign_message(&mut keypair, message.filepack_signed_data())
+    .unwrap();
+
+  let mpi::Signature::EdDSA { r, s } = signature_packet.mpis() else {
+    panic!("expected EdDSA signature");
+  };
+
+  let r_bytes = r.value_padded(32).unwrap();
+  let s_bytes = s.value_padded(32).unwrap();
+
+  let mut sig_bytes = [0u8; 64];
+  sig_bytes[..32].copy_from_slice(&r_bytes);
+  sig_bytes[32..].copy_from_slice(&s_bytes);
+
+  let mpi::PublicKey::EdDSA { q, .. } = signing_key.key().mpis() else {
+    panic!("expected EdDSA public key");
+  };
+
+  let (public_key_bytes, _) = q.decode_point(&openpgp::types::Curve::Ed25519).unwrap();
+
+  signature_packet
+    .clone()
+    .verify_message(signing_key.key(), message.filepack_signed_data())
+    .unwrap();
+
+  {
+    let hashed_area = signature_packet.hashed_area().to_vec().unwrap();
+    let hashed_area_len = hashed_area.len();
+
+    let mut header = [0u8; 6];
+    header[0] = 4;
+    header[1] = u8::from(signature_packet.typ());
+    header[2] = u8::from(signature_packet.pk_algo());
+    header[3] = u8::from(signature_packet.hash_algo());
+    header[4..6].copy_from_slice(&(hashed_area_len as u16).to_be_bytes());
+
+    let mut trailer = [0u8; 6];
+    trailer[0] = 4;
+    trailer[1] = 0xff;
+    let len = (header.len() + hashed_area_len) as u32;
+    trailer[2..6].copy_from_slice(&len.to_be_bytes());
+
+    let mut hasher = Sha512::new();
+    hasher.update(message.filepack_signed_data());
+    hasher.update(&header);
+    hasher.update(&hashed_area);
+    hasher.update(&trailer);
+    let digest = hasher.finalize();
+
+    let verifying_key =
+      ed25519_dalek::VerifyingKey::from_bytes(public_key_bytes.try_into().unwrap()).unwrap();
+    let sig = ed25519_dalek::Signature::from_bytes(&sig_bytes);
+    verifying_key.verify_strict(&digest, &sig).unwrap();
+  }
+
+  let public_key = PublicKey::from_bytes(public_key_bytes.try_into().unwrap());
+
+  let signature = Signature::new(
+    SignatureScheme::Pgp,
+    ed25519_dalek::Signature::from_bytes(&sig_bytes),
+  );
+
+  signature.verify(&message, public_key).unwrap();
 }
