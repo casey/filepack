c3e3805732420bb94a9ba1ce537b916a8786354c
Casey Rodarmor <casey@rodarmor.com>
2026-01-21 12:42:27 -0800

    Tweak

--- src/signature_error.rs
+++ src/signature_error.rs
@@ -7,7 +7,10 @@ pub enum SignatureError {
   Bech32m { source: Bech32mError },
   #[snafu(display("signature scheme `{scheme}` does not support suffix data"))]
   SuffixNotAllowed { scheme: &'static str },
-  #[snafu(display("signature scheme `{scheme}` suffix length {length} exceeds maximum {}", u16::MAX))]
+  #[snafu(display(
+    "signature scheme `{scheme}` suffix length {length} exceeds maximum {}",
+    u16::MAX,
+  ))]
   SuffixTooLarge { length: usize, scheme: &'static str },
   #[snafu(display("bech32m signature scheme `{scheme}` is not supported"))]
   UnsupportedScheme { scheme: Fe32 },
