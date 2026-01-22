19f624c642e490c136e18d6aaf69d57b2bd66c2f
Casey Rodarmor <casey@rodarmor.com>
2026-01-21 18:57:04 -0800

    Adapt

---
+++ src/bech32m_type.rs
@@ -0,0 +1,36 @@
+use super::*;
+
+#[derive(Clone, Copy, Debug)]
+pub enum Bech32mType {
+  Fingerprint,
+  PrivateKey,
+  PublicKey,
+  Signature,
+}
+
+impl Bech32mType {
+  pub(crate) fn hrp(self) -> &'static Hrp {
+    static FINGERPRINT: Hrp = Hrp::parse_unchecked("package");
+    static PRIVATE_KEY: Hrp = Hrp::parse_unchecked("private");
+    static PUBLIC_KEY: Hrp = Hrp::parse_unchecked("public");
+    static SIGNATURE: Hrp = Hrp::parse_unchecked("signature");
+
+    match self {
+      Self::Fingerprint => &FINGERPRINT,
+      Self::PrivateKey => &PRIVATE_KEY,
+      Self::PublicKey => &PUBLIC_KEY,
+      Self::Signature => &SIGNATURE,
+    }
+  }
+}
+
+impl Display for Bech32mType {
+  fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+    match self {
+      Self::Fingerprint => write!(f, "package fingerprint"),
+      Self::PrivateKey => write!(f, "private key"),
+      Self::PublicKey => write!(f, "public key"),
+      Self::Signature => write!(f, "signature"),
+    }
+  }
+}
