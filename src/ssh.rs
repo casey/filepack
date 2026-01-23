84d8f5719fa43aee59d60e2cda56df229e1ef5ad
Casey Rodarmor <casey@rodarmor.com>
2026-01-22 17:57:57 -0800

    Test signatures generated with ssh-key (#213)

--- src/ssh.rs
+++ src/ssh.rs
@@ -1,7 +1,7 @@
 use super::*;
 
 #[test]
-fn ssh_signatures_can_be_verified() {
+fn ssh_keygen_signatures_can_be_verified() {
   let message_bytes = include_bytes!("../static/ssh-test/message");
   let signature_str = include_str!("../static/ssh-test/message.sig");
   let public_key_str = include_str!("../static/ssh-test/id_ed25519.pub");
@@ -48,4 +48,49 @@ fn ssh_signatures_can_be_verified() {
   eprintln!("SSH_PRIVATE_KEY: {}", private_key.display_secret());
   eprintln!("SSH_PUBLIC_KEY: {public_key}");
   eprintln!("SSH_SIGNATURE: {signature}");
+}
+
+#[test]
+fn ssh_signatures_can_be_generated_and_verified() {
+  use {
+    rand::rngs::OsRng,
+    ssh_key::{Algorithm, HashAlg},
+  };
+
+  let message = {
+    let manifest = Manifest {
+      files: Directory::new(),
+      notes: Vec::new(),
+    };
+
+    Message {
+      fingerprint: manifest.fingerprint(),
+      time: None,
+    }
+    .serialize()
+  };
+
+  let private_key = ssh_key::PrivateKey::random(&mut OsRng, Algorithm::Ed25519).unwrap();
+
+  let ssh_sig = private_key
+    .sign("filepack", HashAlg::Sha512, message.as_bytes())
+    .unwrap();
+
+  let signature = {
+    let sig_bytes: [u8; 64] = ssh_sig.signature_bytes().try_into().unwrap();
+    Signature::new(
+      SignatureScheme::Ssh,
+      ed25519_dalek::Signature::from_bytes(&sig_bytes),
+    )
+  };
+
+  let public_key = {
+    let ssh_key::public::KeyData::Ed25519(ed25519_key) = private_key.public_key().key_data() else {
+      panic!("expected ed25519");
+    };
+
+    PublicKey::from_bytes(ed25519_key.0)
+  };
+
+  signature.verify(&message, public_key).unwrap();
 }
