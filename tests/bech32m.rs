095097939e16491d4eeeaa5dace686a7d9ec1c0e
Casey Rodarmor <casey@rodarmor.com>
2026-01-22 17:15:06 -0800

    Modify

--- tests/bech32m.rs
+++ tests/bech32m.rs
@@ -17,6 +17,22 @@ fn decode_invalid_bech32m() {
 }
 
 #[test]
+fn decode_prefix_missing() {
+  Test::new()
+    .args(["bech32m", "--decode", "foo1vehk7e3xjxp", "--prefix", "q"])
+    .stderr("error: bech32m prefix missing\n")
+    .failure();
+}
+
+#[test]
+fn decode_with_prefix() {
+  Test::new()
+    .args(["bech32m", "--decode", "foo1qvehk7ml2uzp", "--prefix", "q"])
+    .stdout("666f6f\n")
+    .success();
+}
+
+#[test]
 fn encode() {
   Test::new()
     .args(["bech32m", "--encode", "666f6f", "--hrp", "foo"])
@@ -41,20 +57,20 @@ fn encode_invalid_hrp() {
 }
 
 #[test]
+fn encode_invalid_prefix() {
+  Test::new()
+    .args([
+      "bech32m", "--encode", "666f6f", "--hrp", "foo", "--prefix", "!",
+    ])
+    .stderr_regex("error: invalid bech32m prefix character `!`\n.*")
+    .failure();
+}
+
+#[test]
 fn encode_requires_hrp() {
   Test::new()
     .args(["bech32m", "--encode", "666f6f"])
     .stderr_regex("error: the following required arguments were not provided:.*--hrp.*")
-    .status(2);
-}
-
-#[test]
-fn source_required() {
-  Test::new()
-    .arg("bech32m")
-    .stderr_regex(
-      "error: the following required arguments were not provided:.*--decode.*--encode.*",
-    )
     .status(2);
 }
 
@@ -69,27 +85,11 @@ fn encode_with_prefix() {
 }
 
 #[test]
-fn decode_with_prefix() {
+fn source_required() {
   Test::new()
-    .args(["bech32m", "--decode", "foo1qvehk7ml2uzp", "--prefix", "q"])
-    .stdout("666f6f\n")
-    .success();
-}
-
-#[test]
-fn decode_prefix_missing() {
-  Test::new()
-    .args(["bech32m", "--decode", "foo1vehk7e3xjxp", "--prefix", "q"])
-    .stderr("error: bech32m prefix missing\n")
-    .failure();
-}
-
-#[test]
-fn encode_invalid_prefix() {
-  Test::new()
-    .args([
-      "bech32m", "--encode", "666f6f", "--hrp", "foo", "--prefix", "!",
-    ])
-    .stderr_regex("error: invalid bech32m prefix character `!`\n.*")
-    .failure();
+    .arg("bech32m")
+    .stderr_regex(
+      "error: the following required arguments were not provided:.*--decode.*--encode.*",
+    )
+    .status(2);
 }
