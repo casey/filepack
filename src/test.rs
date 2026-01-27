de644eb657863016f37f4072c2c9580a0ee6b28d
Casey Rodarmor <casey@rodarmor.com>
2026-01-26 19:16:26 -0800

    Fix some tests

--- src/test.rs
+++ src/test.rs
@@ -12,8 +12,8 @@ pub(crate) const PUBLIC_KEY: &str =
   "public1a67dndhhmae7p6fsfnj0z37zf78cde6mwqgtms0y87h8ldlvvflyqcxnd63";
 
 pub(crate) const SIGNATURE: &str = concat!(
-  "signature1af0qppampjlm7qs0g4amn9fnq87crhn70k5lv5wf48ajy6k774tq",
-  "w6yc9s5n0kpq5420jrz644sgu7geahpffl8l7nuv9azsqv8jpgtrcqstq8xwz",
+  "signature1appampjlm7qs0g4amn9fnq87crhn70k5lv5wf48ajy6k774tqw6yc9s5n0kpq5420jrz64",
+  "4sgu7geahpffl8l7nuv9azsqv8jpgtrcqsfz5zpg",
 );
 
 pub(crate) const WEAK_PUBLIC_KEY: &str =
