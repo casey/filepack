15686aac44c5fa67b14d4849c785d97839be049d
Casey Rodarmor <casey@rodarmor.com>
2026-04-17 11:40:08 -0700

    Adjust

--- src/cbor/decode_error.rs
+++ src/cbor/decode_error.rs
@@ -3,7 +3,7 @@ use super::*;
 #[derive(Debug, PartialEq, Snafu)]
 #[snafu(context(suffix(false)), visibility(pub(crate)))]
 pub(crate) enum DecodeError {
-  #[snafu(display("failed to decode component"))]
+  #[snafu(display("failed to parse component"))]
   Component { source: ComponentError },
   #[snafu(display("integer out of range"))]
   IntegerRange { source: TryFromIntError },
