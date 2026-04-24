1c8aac607cf427a134b07688e89f76ea38531ce0
Casey Rodarmor <casey@rodarmor.com>
2026-04-23 21:11:06 -0700

    Adjust

--- crates/filepack-cbor/src/lib.rs
+++ crates/filepack-cbor/src/lib.rs
@@ -8,16 +8,16 @@ use {
 #[derive(FromDeriveInput)]
 #[darling(supports(struct_named))]
 struct Input {
-  ident: syn::Ident,
   data: ast::Data<(), Field>,
+  ident: syn::Ident,
 }
 
 #[derive(FromField)]
 #[darling(forward_attrs(n))]
 struct Field {
+  attrs: Vec<syn::Attribute>,
   ident: Option<syn::Ident>,
   ty: syn::Type,
-  attrs: Vec<syn::Attribute>,
 }
 
 struct ParsedField<'a> {
@@ -36,7 +36,7 @@ fn parse_n(field: &Field) -> syn::Result<u8> {
       if n.is_some() {
         return Err(syn::Error::new_spanned(attr, "duplicate #[n] attribute"));
       }
-      let lit: LitInt = attr.parse_args()?;
+      let lit = attr.parse_args::<LitInt>()?;
       n = Some(lit.base10_parse::<u8>()?);
     }
   }
@@ -53,44 +53,6 @@ fn is_option(ty: &syn::Type) -> bool {
   false
 }
 
-fn parse_fields(input: &Input) -> syn::Result<Vec<ParsedField<'_>>> {
-  let fields = input.data.as_ref().take_struct().unwrap();
-
-  let mut parsed = Vec::new();
-
-  for field in fields {
-    let ident = field.ident.as_ref().unwrap();
-    let n = parse_n(field)?;
-    let optional = is_option(&field.ty);
-    parsed.push(ParsedField { ident, n, optional });
-  }
-
-  parsed.sort_by_key(|f| f.n);
-
-  for window in parsed.windows(2) {
-    if window[0].n == window[1].n {
-      return Err(syn::Error::new_spanned(
-        window[1].ident,
-        format!("duplicate key {}", window[1].n),
-      ));
-    }
-  }
-
-  for (i, field) in parsed.iter().enumerate() {
-    if field.n != i as u8 {
-      return Err(syn::Error::new_spanned(
-        field.ident,
-        format!(
-          "keys must be contiguous starting from 0: expected {i}, found {}",
-          field.n
-        ),
-      ));
-    }
-  }
-
-  Ok(parsed)
-}
-
 #[proc_macro_derive(Encode, attributes(n))]
 pub fn derive_encode(input: TokenStream) -> TokenStream {
   let input = syn::parse_macro_input!(input as DeriveInput);
@@ -100,42 +62,10 @@ pub fn derive_encode(input: TokenStream) -> TokenStream {
     Err(err) => return err.write_errors().into(),
   };
 
-  match derive_encode_inner(&input) {
+  match input.derive_encode_inner() {
     Ok(tokens) => tokens.into(),
     Err(err) => err.to_compile_error().into(),
   }
-}
-
-fn derive_encode_inner(input: &Input) -> syn::Result<proc_macro2::TokenStream> {
-  let name = &input.ident;
-  let fields = parse_fields(input)?;
-
-  let required_count = fields.iter().filter(|f| !f.optional).count() as u64;
-
-  let count_optionals = fields.iter().filter(|f| f.optional).map(|f| {
-    let ident = f.ident;
-    quote! { + u64::from(self.#ident.is_some()) }
-  });
-
-  let items = fields.iter().map(|f| {
-    let ident = f.ident;
-    let n = f.n;
-    if f.optional {
-      quote! { map.optional_item(#n, self.#ident.as_ref()); }
-    } else {
-      quote! { map.item(#n, &self.#ident); }
-    }
-  });
-
-  Ok(quote! {
-    impl crate::Encode for #name {
-      fn encode(&self, encoder: &mut crate::Encoder) {
-        let length = #required_count #(#count_optionals)*;
-        let mut map = encoder.map::<u8>(length);
-        #(#items)*
-      }
-    }
-  })
 }
 
 #[proc_macro_derive(Decode, attributes(n))]
@@ -147,38 +77,110 @@ pub fn derive_decode(input: TokenStream) -> TokenStream {
     Err(err) => return err.write_errors().into(),
   };
 
-  match derive_decode_inner(&input) {
+  match input.derive_decode_inner() {
     Ok(tokens) => tokens.into(),
     Err(err) => err.to_compile_error().into(),
   }
 }
 
-fn derive_decode_inner(input: &Input) -> syn::Result<proc_macro2::TokenStream> {
-  let name = &input.ident;
-  let fields = parse_fields(input)?;
+impl Input {
+  fn derive_decode_inner(&self) -> syn::Result<proc_macro2::TokenStream> {
+    let name = &self.ident;
+    let fields = self.parse_fields()?;
 
-  let decode_fields = fields.iter().map(|f| {
-    let ident = f.ident;
-    let n = f.n;
-    if f.optional {
-      quote! { let #ident = map.optional_key(#n)?; }
-    } else {
-      quote! { let #ident = map.required_key(#n)?; }
+    let decode_fields = fields.iter().map(|f| {
+      let ident = f.ident;
+      let n = f.n;
+      if f.optional {
+        quote! { let #ident = map.optional_key(#n)?; }
+      } else {
+        quote! { let #ident = map.required_key(#n)?; }
+      }
+    });
+
+    let field_names = fields.iter().map(|f| f.ident);
+
+    Ok(quote! {
+      impl crate::Decode for #name {
+        fn decode(decoder: &mut crate::Decoder) -> Result<Self, crate::DecodeError> {
+          let mut map = decoder.map::<u8>()?;
+          #(#decode_fields)*
+          map.finish()?;
+          Ok(Self {
+            #(#field_names,)*
+          })
+        }
+      }
+    })
+  }
+
+  fn parse_fields(&self) -> syn::Result<Vec<ParsedField<'_>>> {
+    let fields = self.data.as_ref().take_struct().unwrap();
+
+    let mut parsed = Vec::new();
+
+    for field in fields {
+      let ident = field.ident.as_ref().unwrap();
+      let n = parse_n(field)?;
+      let optional = is_option(&field.ty);
+      parsed.push(ParsedField { ident, n, optional });
     }
-  });
 
-  let field_names = fields.iter().map(|f| f.ident);
+    parsed.sort_by_key(|f| f.n);
 
-  Ok(quote! {
-    impl crate::Decode for #name {
-      fn decode(decoder: &mut crate::Decoder) -> Result<Self, crate::DecodeError> {
-        let mut map = decoder.map::<u8>()?;
-        #(#decode_fields)*
-        map.finish()?;
-        Ok(Self {
-          #(#field_names,)*
-        })
+    for window in parsed.windows(2) {
+      if window[0].n == window[1].n {
+        return Err(syn::Error::new_spanned(
+          window[1].ident,
+          format!("duplicate key {}", window[1].n),
+        ));
       }
     }
-  })
+
+    for (i, field) in parsed.iter().enumerate() {
+      if field.n != i as u8 {
+        return Err(syn::Error::new_spanned(
+          field.ident,
+          format!(
+            "keys must be contiguous starting from 0: expected {i}, found {}",
+            field.n
+          ),
+        ));
+      }
+    }
+
+    Ok(parsed)
+  }
+
+  fn derive_encode_inner(&self) -> syn::Result<proc_macro2::TokenStream> {
+    let name = &self.ident;
+    let fields = self.parse_fields()?;
+
+    let required_count = fields.iter().filter(|f| !f.optional).count() as u64;
+
+    let count_optionals = fields.iter().filter(|f| f.optional).map(|f| {
+      let ident = f.ident;
+      quote! { + u64::from(self.#ident.is_some()) }
+    });
+
+    let items = fields.iter().map(|f| {
+      let ident = f.ident;
+      let n = f.n;
+      if f.optional {
+        quote! { map.optional_item(#n, self.#ident.as_ref()); }
+      } else {
+        quote! { map.item(#n, &self.#ident); }
+      }
+    });
+
+    Ok(quote! {
+      impl crate::Encode for #name {
+        fn encode(&self, encoder: &mut crate::Encoder) {
+          let length = #required_count #(#count_optionals)*;
+          let mut map = encoder.map::<u8>(length);
+          #(#items)*
+        }
+      }
+    })
+  }
 }
