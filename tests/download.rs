c2b2475b44c3624c2984b09689a7e8fdeed07013
Casey Rodarmor <casey@rodarmor.com>
2026-05-11 17:12:58 -0700

    Reform

--- tests/download.rs
+++ tests/download.rs
@@ -4,6 +4,7 @@ use super::*;
 fn download_retrieves_file() {
   let node = Test::new()
     .args(["node", "--ready-fd", "3", "127.0.0.1:0"])
+    .assert_file(&format!("files/{}", Hash::bytes(b"bar")), "bar")
     .ready_fd()
     .spawn();
 
