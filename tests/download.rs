3a6da4832d4e4c22fe4eee43bde0e4122aa748d5
Casey Rodarmor <casey@rodarmor.com>
2026-05-15 18:14:20 -0700

    Reform

--- tests/download.rs
+++ tests/download.rs
@@ -32,34 +32,6 @@ fn download_fails_if_output_already_exists() {
 }
 
 #[test]
-fn download_fails_with_404_when_file_missing() {
-  let server = Test::new()
-    .args(["serve", "--address", "127.0.0.1:0"])
-    .ready_fd()
-    .spawn();
-
-  let hash = Hash::bytes(b"bar");
-
-  Test::new()
-    .args([
-      "download",
-      "--server",
-      &server.address(),
-      "--hash",
-      &hash.to_string(),
-      "--output",
-      "foo",
-    ])
-    .stderr(&format!(
-      "error: response from {}/{hash} failed with status 404 Not Found: file with hash f2e897eed7d206cd855d441598fa521abc75aa96953e97c030c9612c30c1293d not found\n",
-      server.address(),
-    ))
-    .failure();
-
-  server.terminate().success();
-}
-
-#[test]
 fn download_fails_on_hash_mismatch() {
   let expected = Hash::bytes(b"baz");
   let actual = Hash::bytes(b"bar");
@@ -82,6 +54,34 @@ fn download_fails_on_hash_mismatch() {
     ])
     .stderr(&format!(
       "error: downloaded file hash mismatch: expected {expected} but got {actual}\n",
+    ))
+    .failure();
+
+  server.terminate().success();
+}
+
+#[test]
+fn download_fails_with_404_when_file_missing() {
+  let server = Test::new()
+    .args(["serve", "--address", "127.0.0.1:0"])
+    .ready_fd()
+    .spawn();
+
+  let hash = Hash::bytes(b"bar");
+
+  Test::new()
+    .args([
+      "download",
+      "--server",
+      &server.address(),
+      "--hash",
+      &hash.to_string(),
+      "--output",
+      "foo",
+    ])
+    .stderr(&format!(
+      "error: response from {}/{hash} failed with status 404 Not Found: file with hash f2e897eed7d206cd855d441598fa521abc75aa96953e97c030c9612c30c1293d not found\n",
+      server.address(),
     ))
     .failure();
 
