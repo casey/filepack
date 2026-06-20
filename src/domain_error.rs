cdcd137bf4d2813649507ce2e96d858125e45f49
Casey Rodarmor <casey@rodarmor.com>
2026-06-19 23:30:38 -0700

    Amend

--- src/domain_error.rs
+++ src/domain_error.rs
@@ -5,13 +5,13 @@ use super::*;
 pub enum DomainError {
   #[snafu(display("domain empty"))]
   Empty,
-  #[snafu(display("domain of {len} bytes exceeds maximum of 253"))]
+  #[snafu(display("domain length {len} exceeds maximum of 253 bytes"))]
   Length { len: usize },
   #[snafu(display("domain must have at least two labels"))]
   TooFewLabels,
   #[snafu(display("domain contains empty label"))]
   EmptyLabel,
-  #[snafu(display("label of {len} bytes exceeds maximum of 63"))]
+  #[snafu(display("label length {len} exceeds maximum of 63 bytes"))]
   LabelLength { len: usize },
   #[snafu(display("invalid character {} in domain", c.escape_debug()))]
   Character { c: char },
