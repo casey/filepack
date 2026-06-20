b57fb6518b9202441fba7eaeae6083deb87b5ef8
Casey Rodarmor <casey@rodarmor.com>
2026-06-20 15:25:23 -0700

    Enhance

--- static/index.js
+++ static/index.js
@@ -1,54 +1,57 @@
 const style = new CSSStyleSheet()
 
 style.replaceSync(`
-  button {
-    background: none;
-    border: none;
-    color: inherit;
-    cursor: pointer;
-    height: 1rem;
-    padding: 0;
-    width: 1rem;
-  }
+button {
+  background: none;
+  border: none;
+  color: inherit;
+  cursor: pointer;
+  height: 1rem;
+  padding: 0;
+  width: 1rem;
+}
 
-  button:hover {
-    text-shadow: 0 0 5px #fff;
-  }
-`)
+button:hover {
+  text-shadow: 0 0 5px #fff;
+}
+`);
 
 const template = document.createElement('template')
-template.innerHTML = '<button>▶</button><audio></audio>'
+template.innerHTML = `
+  <button>▶</button>
+  <audio></audio>
+`;
 
 class PlayButton extends HTMLElement {
   static get observedAttributes() {
-    return ['src']
+    return ['src'];
   }
 
   constructor() {
-    super()
+    super();
 
-    const shadow = this.attachShadow({ mode: 'open' })
-    shadow.adoptedStyleSheets = [style]
-    shadow.append(template.content.cloneNode(true))
+    const shadow = this.attachShadow({ mode: 'open' });
+    shadow.adoptedStyleSheets = [style];
+    shadow.append(template.content.cloneNode(true));
 
-    const audio = shadow.querySelector('audio')
-    const button = shadow.querySelector('button')
+    const audio = shadow.querySelector('audio');
+    const button = shadow.querySelector('button');
 
     button.addEventListener('click', () => {
       if (audio.paused) {
-        audio.play()
+        audio.play();
       } else {
-        audio.pause()
+        audio.pause();
       }
-    })
+    });
 
     audio.addEventListener('play', () => {
       button.textContent = '⏸'
-    })
+    });
 
     audio.addEventListener('pause', () => {
       button.textContent = '▶'
-    })
+    });
   }
 
   attributeChangedCallback(name, old, value) {
