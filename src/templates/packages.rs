938c702d6024a433d17f12bc295d5a8623a6f14b
Casey Rodarmor <casey@rodarmor.com>
2026-07-20 14:07:34 -0700

    Add packages method

--- src/templates/packages.rs
+++ src/templates/packages.rs
@@ -5,6 +5,24 @@ pub(crate) struct PackagesHtml {
   pub(crate) packages: Vec<(Fingerprint, Option<Metadata>)>,
 }
 
+impl PackagesHtml {
+  fn packages(
+    &self,
+  ) -> impl Iterator<Item = (Fingerprint, Option<&ComponentBuf>, Option<&ComponentBuf>)> {
+    self.packages.iter().map(|(fingerprint, metadata)| {
+      (
+        *fingerprint,
+        metadata
+          .as_ref()
+          .and_then(|metadata| metadata.creator.as_ref()),
+        metadata
+          .as_ref()
+          .and_then(|metadata| metadata.title.as_ref()),
+      )
+    })
+  }
+}
+
 impl Page for PackagesHtml {
   fn title(&self) -> String {
     "packages · filepack".into()
