7d33102b863876fe0b4785ad0029a0475482bc05
Casey Rodarmor <casey@rodarmor.com>
2026-07-20 19:29:06 -0700

    Adjust

--- src/video_type.rs
+++ src/video_type.rs
@@ -35,12 +35,6 @@ mod tests {
   use super::*;
 
   #[test]
-  fn encoding() {
-    assert_cbor(VideoType::Mp4, "00");
-    assert_cbor(VideoType::Webm, "01");
-  }
-
-  #[test]
   fn from_extension() {
     assert_eq!(VideoType::from_extension("mp4"), Some(VideoType::Mp4));
     assert_eq!(VideoType::from_extension("webm"), Some(VideoType::Webm));
