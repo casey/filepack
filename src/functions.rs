b49a74941b0f5084869f5928fe6659fc8094d4f9
Casey Rodarmor <casey@rodarmor.com>
2026-08-14 15:28:20 -0700

    Add dedicated server client

--- src/functions.rs
+++ src/functions.rs
@@ -1,21 +1,4 @@
-use {super::*, reqwest::blocking::RequestBuilder, url::Host};
-
-pub(crate) fn client() -> Result<Client> {
-  install_default_crypto_provider()?;
-
-  Client::builder()
-    .connect_timeout(Duration::from_secs(30))
-    .http2_adaptive_window(true)
-    .tcp_keepalive(Duration::from_secs(30))
-    .timeout(None::<Duration>)
-    .user_agent(concat!(
-      env!("CARGO_PKG_NAME"),
-      "/",
-      env!("CARGO_PKG_VERSION")
-    ))
-    .build()
-    .context(error::ClientBuild)
-}
+use super::*;
 
 pub(crate) fn current_dir() -> Result<Utf8PathBuf> {
   Utf8PathBuf::from_path_buf(env::current_dir().context(error::CurrentDir)?)
@@ -51,30 +34,6 @@ pub(crate) fn is_lowercase_hex(s: &str) -> bool {
     .all(|c| c.is_ascii_hexdigit() && (c.is_numeric() || c.is_lowercase()))
 }
 
-pub(crate) fn load_auth_key(
-  options: &Options,
-  server: &Url,
-  auth: Option<&KeyName>,
-) -> Result<Option<PrivateKey>> {
-  let Some(name) = auth else {
-    return Ok(None);
-  };
-
-  let loopback = match server.host().unwrap() {
-    Host::Domain(domain) => domain == "localhost",
-    Host::Ipv4(addr) => addr.is_loopback(),
-    Host::Ipv6(addr) => addr.is_loopback(),
-  };
-
-  ensure!(server.scheme() == "https" || loopback, error::TokenOverHttp);
-
-  let keychain = Keychain::load(options)?;
-
-  Ok(Some(PrivateKey::load(
-    &keychain.path.join(name.private_key_filename()),
-  )?))
-}
-
 pub(crate) fn now() -> Result<u64> {
   Ok(
     SystemTime::now()
@@ -91,19 +50,6 @@ pub(crate) fn parse_number<T: FromStr<Err = ParseIntError>>(s: &str) -> Result<T
   }
 
   Ok(s.parse()?)
-}
-
-pub(crate) fn request_with_token(
-  mut builder: RequestBuilder,
-  server: &Url,
-  key: Option<&PrivateKey>,
-) -> Result<RequestBuilder> {
-  if let Some(key) = key {
-    let host = server.host_str().unwrap().to_owned();
-    builder = builder.bearer_auth(Token::encode(key, &host)?);
-  }
-
-  Ok(builder)
 }
 
 pub(crate) fn transfer_tempfile(hash: Hash, path: &Utf8Path) -> io::Result<NamedTempFile> {
