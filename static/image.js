5695f807551bf5d810d2c17363f358b12eea72d1
Casey Rodarmor <casey@rodarmor.com>
2026-08-20 23:57:22 -0700

    Don't listen for resize events

--- static/image.js
+++ static/image.js
@@ -1,24 +1,12 @@
-function fit() {
-  let scale = Math.min(
-    document.documentElement.clientWidth / width,
-    document.documentElement.clientHeight / height,
-  );
-  img.style.width = `${width * scale}px`;
-}
-
-document.documentElement.classList.add('js');
-
 let img = document.querySelector('img');
 let width = Number(img.getAttribute('width'));
 let height = Number(img.getAttribute('height'));
-let ratio = devicePixelRatio;
 
-fit();
+let scale = Math.min(
+  document.documentElement.clientWidth / width,
+  document.documentElement.clientHeight / height,
+);
 
-addEventListener('resize', () => {
-  if (devicePixelRatio === ratio) {
-    fit();
-  } else {
-    ratio = devicePixelRatio;
-  }
-});
+document.documentElement.classList.add('js');
+
+img.style.width = `${width * scale}px`;
