f0e1fad6b097cb5c6ff5fa0b4887dfb8d60afa97
Casey Rodarmor <casey@rodarmor.com>
2026-08-23 17:22:44 -0700

    Add `all` lint group (#560)

--- src/lint_group.rs
+++ src/lint_group.rs
@@ -3,6 +3,7 @@ use super::*;
 #[derive(Clone, Copy, EnumIter, Eq, Ord, PartialEq, PartialOrd, Serialize, ValueEnum)]
 #[serde(rename_all = "kebab-case")]
 pub(crate) enum LintGroup {
+  All,
   Compatibility,
   Content,
   Distribution,
@@ -10,10 +11,19 @@ pub(crate) enum LintGroup {
 }
 
 impl LintGroup {
+  #[cfg(test)]
+  fn is_superset(self) -> bool {
+    match self {
+      Self::All | Self::Distribution => true,
+      Self::Compatibility | Self::Content | Self::Junk => false,
+    }
+  }
+
   pub(crate) fn lints(self) -> BTreeSet<Lint> {
     use Lint::*;
 
     match self {
+      Self::All => Lint::iter().collect(),
       Self::Compatibility => [
         CaseConflict,
         WindowsLeadingSpace,
@@ -35,14 +45,28 @@ mod tests {
   use super::*;
 
   #[test]
+  fn all_lints_are_in_all_group() {
+    let all = LintGroup::All.lints();
+    for lint in Lint::iter() {
+      assert!(all.contains(&lint), "lint `{lint}` not in `all` lint group");
+    }
+  }
+
+  #[test]
   fn all_lints_are_in_at_least_one_group() {
     let mut lints = BTreeSet::new();
     for group in LintGroup::iter() {
-      lints.append(&mut group.lints());
+      let group_lints = group.lints();
+
+      if group.is_superset() {
+        continue;
+      }
+
+      lints.extend(group_lints);
     }
 
     for lint in Lint::iter() {
-      assert!(lints.contains(&lint), "lint {lint} not in group");
+      assert!(lints.contains(&lint), "lint `{lint}` not in group");
     }
   }
 }
