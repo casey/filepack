1bf6f4a84591fe504ffa02701d88dffb5cd018b9
Casey Rodarmor <casey@rodarmor.com>
2026-08-25 16:22:12 -0700

    Use `ProgressBar` wrapper (#577)

--- src/progress_bar.rs
+++ src/progress_bar.rs
@@ -1,4 +1,7 @@
-use super::*;
+use {
+  super::*,
+  indicatif::{ProgressBarIter, ProgressStyle},
+};
 
 const COUNT_TEMPLATE: &str = "{spinner:.green} ⟪{elapsed_precise}⟫ ⟦{wide_bar:.cyan}⟧ \
                               {pos}/{len} {msg} ⟨{eta}⟩";
@@ -48,56 +51,91 @@ const TICK_CHARS: &str = concat!(
   "⠛⠚⠓⠒⠙⠘⠑⠐⠋⠊⠃⠂⠉⠈⠁",
 );
 
-pub(crate) fn count(quiet: bool, len: u64, message: &'static str) -> ProgressBar {
-  if quiet {
-    ProgressBar::hidden()
-  } else {
-    ProgressBar::new(len)
-      .with_style(
-        ProgressStyle::default_bar()
-          .progress_chars(PROGRESS_CHARS)
-          .template(COUNT_TEMPLATE)
-          .unwrap()
-          .tick_chars(TICK_CHARS),
-      )
-      .with_message(message)
+struct Items {
+  done: u64,
+  noun: &'static str,
+  total: u64,
+}
+
+impl Display for Items {
+  fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+    write!(f, "{}/{} {}", self.done, self.total, self.noun)
   }
 }
 
-pub(crate) fn entry_progress_message(downloaded: u64, entries: u64) -> String {
-  format!("{downloaded}/{entries} entries")
+pub(crate) struct ProgressBar {
+  inner: indicatif::ProgressBar,
+  items: Option<Items>,
 }
 
-pub(crate) fn file_progress_message(uploaded: u64, files: u64) -> String {
-  format!("{uploaded}/{files} files")
-}
-
-pub(crate) fn new(options: &Options, bytes: u64) -> ProgressBar {
-  if options.quiet {
-    ProgressBar::hidden()
-  } else {
-    ProgressBar::new(bytes).with_style(
-      ProgressStyle::default_bar()
-        .progress_chars(PROGRESS_CHARS)
-        .template(TEMPLATE)
-        .unwrap()
-        .tick_chars(TICK_CHARS),
-    )
+impl ProgressBar {
+  pub(crate) fn bytes(options: &Options, bytes: u64) -> Self {
+    Self {
+      inner: Self::inner(options.quiet, bytes, TEMPLATE),
+      items: None,
+    }
   }
-}
 
-pub(crate) fn with_message(options: &Options, bytes: u64, message: String) -> ProgressBar {
-  if options.quiet {
-    ProgressBar::hidden()
-  } else {
-    ProgressBar::new(bytes)
-      .with_style(
+  pub(crate) fn count(quiet: bool, len: u64, message: &'static str) -> Self {
+    Self {
+      inner: Self::inner(quiet, len, COUNT_TEMPLATE).with_message(message),
+      items: None,
+    }
+  }
+
+  pub(crate) fn inc(&self, delta: u64) {
+    self.inner.inc(delta);
+  }
+
+  fn inner(quiet: bool, len: u64, template: &str) -> indicatif::ProgressBar {
+    if quiet {
+      indicatif::ProgressBar::hidden()
+    } else {
+      indicatif::ProgressBar::new(len).with_style(
         ProgressStyle::default_bar()
           .progress_chars(PROGRESS_CHARS)
-          .template(TEMPLATE_WITH_MESSAGE)
+          .template(template)
           .unwrap()
           .tick_chars(TICK_CHARS),
       )
-      .with_message(message)
+    }
+  }
+
+  pub(crate) fn item_done(&mut self) {
+    self.items.as_mut().unwrap().done += 1;
+    self.update_message();
+  }
+
+  pub(crate) fn items(options: &Options, bytes: u64, items: u64, noun: &'static str) -> Self {
+    let bar = Self {
+      inner: Self::inner(options.quiet, bytes, TEMPLATE_WITH_MESSAGE),
+      items: Some(Items {
+        done: 0,
+        noun,
+        total: items,
+      }),
+    };
+    bar.update_message();
+    bar
+  }
+
+  pub(crate) fn set_totals(&mut self, bytes: u64, items: u64) {
+    self.inner.set_length(bytes);
+    self.items.as_mut().unwrap().total = items;
+    self.update_message();
+  }
+
+  fn update_message(&self) {
+    self
+      .inner
+      .set_message(self.items.as_ref().unwrap().to_string());
+  }
+
+  pub(crate) fn wrap_read<R: Read>(&self, read: R) -> ProgressBarIter<R> {
+    self.inner.wrap_read(read)
+  }
+
+  pub(crate) fn wrap_write<W: Write>(&self, write: W) -> ProgressBarIter<W> {
+    self.inner.wrap_write(write)
   }
 }
