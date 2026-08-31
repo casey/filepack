a2f8f1a372f2f76cb1d3a1ac0089947e2254d0e6
Casey Rodarmor <casey@rodarmor.com>
2026-08-30 23:26:47 -0700

    Display video images on `/package` (#643)

--- src/dimensions.rs
+++ src/dimensions.rs
@@ -8,8 +8,31 @@ pub struct Dimensions {
   pub(crate) width: u64,
 }
 
+impl Dimensions {
+  pub(crate) fn css_aspect_ratio(self) -> String {
+    format!("{} / {}", self.width.max(1), self.height.max(1))
+  }
+}
+
 impl Display for Dimensions {
   fn fmt(&self, f: &mut Formatter) -> fmt::Result {
     write!(f, "{}×{}", self.width, self.height)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn css_aspect_ratio() {
+    #[track_caller]
+    fn case(width: u64, height: u64, expected: &str) {
+      assert_eq!(Dimensions { height, width }.css_aspect_ratio(), expected);
+    }
+
+    case(2, 1, "2 / 1");
+    case(1, 2, "1 / 2");
+    case(0, 0, "1 / 1");
   }
 }
