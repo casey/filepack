5260f08f5d4f1da2a3de10253776000fd56ce4a8
Casey Rodarmor <casey@rodarmor.com>
2026-09-06 16:20:54 -0700

    Fix shortcuts popover on mobile Safari (#681)

--- src/templates/item.rs
+++ src/templates/item.rs
@@ -346,13 +346,15 @@ mod tests {
 
     let shortcuts = "
             <button aria-label='Keyboard shortcuts' popovertarget=shortcuts title='Keyboard shortcuts'>?</button>
-            <dl id=shortcuts popover>
-              <div><dt><kbd>p</kbd></dt><dd>go to previous item</dd></div>
-              <div><dt><kbd>n</kbd></dt><dd>go to next item</dd></div>
-              <div><dt><kbd>u</kbd></dt><dd>go to package</dd></div>
-              <div><dt><kbd>f</kbd></dt><dd>toggle full screen</dd></div>
-              <div><dt><kbd>?</kbd></dt><dd>toggle shortcuts</dd></div>
-            </dl>
+            <div id=shortcuts popover>
+              <dl>
+                <div><dt><kbd>p</kbd></dt><dd>go to previous item</dd></div>
+                <div><dt><kbd>n</kbd></dt><dd>go to next item</dd></div>
+                <div><dt><kbd>u</kbd></dt><dd>go to package</dd></div>
+                <div><dt><kbd>f</kbd></dt><dd>toggle full screen</dd></div>
+                <div><dt><kbd>?</kbd></dt><dd>toggle shortcuts</dd></div>
+              </dl>
+            </div>
     "
     .trim();
 
