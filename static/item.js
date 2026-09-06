bd5f4005f5750584d4946e79c6964092cc4ac4ec
Casey Rodarmor <casey@rodarmor.com>
2026-09-06 14:21:55 -0700

    Add popover and fullscreen toggle keyboard shortcuts

--- static/item.js
+++ static/item.js
@@ -1,9 +1,25 @@
-let prev = document.querySelector('link[rel=prev]');
+let media = document.querySelector('img, video');
 let next = document.querySelector('link[rel=next]');
+let prev = document.querySelector('link[rel=prev]');
+let shortcuts = document.querySelector('#shortcuts');
 let up = document.querySelector('link[rel=up]');
 
 document.addEventListener('keydown', (event) => {
-  if (event.altKey || event.ctrlKey || event.metaKey || event.shiftKey) {
+  if (event.altKey || event.ctrlKey || event.metaKey) {
+    return;
+  }
+
+  if (event.key === '?') {
+    shortcuts.togglePopover();
+    return;
+  }
+
+  if (event.key === 'f' && document.fullscreenEnabled) {
+    if (document.fullscreenElement === null) {
+      media.requestFullscreen();
+    } else {
+      document.exitFullscreen();
+    }
     return;
   }
 
